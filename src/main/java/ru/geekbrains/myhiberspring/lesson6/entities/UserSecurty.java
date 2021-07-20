c9800845cdbe1ad9b54555290b18626dc25eb9d8
Vasiliy Boynov <bot52@yandex.ru>
2021-07-20 23:32:25 +0300

    add Lesson6

---
+++ src/main/java/ru/geekbrains/myhiberspring/lesson6/entities/UserSecurty.java
@@ -0,0 +1,44 @@
+package ru.geekbrains.myhiberspring.lesson6.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "user_security")
+public class UserSecurty {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @OneToOne(mappedBy = "securityInfo")
+    private User user;
+
+    @Override
+    public String toString() {
+        return "UserSecurty{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", user=" + user +
+                '}';
+    }
+}
